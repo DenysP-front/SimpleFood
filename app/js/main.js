b67df2172bcdc822414172426c1066da206b5413
Denys <den.popov1002@gmail.com>
2022-10-21 12:11:46 +0300

    13 homework

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,8 @@
 $(function () {
 
+    $('.reviews-slider').slick({
+        arrows: true,
+        dots:true
+    });
    
 });
--- gulpfile.js
+++ gulpfile.js
@@ -34,7 +34,7 @@ function browsersync() {
 
 function styles() {
     return src('app/scss/style.scss')
-    .pipe(scss({outputStyle: 'compressed'}))
+    .pipe(scss({outputStyle: 'expanded'}))
     .pipe(concat('style.min.css'))
     .pipe(autoprefixer({
         overrideBrowserslist: ['last 10 versions'],
@@ -48,6 +48,7 @@ function styles() {
 function scripts () {
     return src([
         'node_modules/jquery/dist/jquery.js',
+        'node_modules/slick-carousel/slick/slick.js',
         'app/js/main.js'
     ])
     .pipe(concat('main.min.js'))
